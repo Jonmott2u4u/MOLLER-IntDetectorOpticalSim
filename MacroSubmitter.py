4db90cb3e12ce23d808c9e1c7a145fad451ff4a1
Jonmott2u4u <67879766+Jonmott2u4u@users.noreply.github.com>
2023-10-27 09:25:42 -0400

    Added a loop for the segment cut parameter

--- MacroSubmitter.py
+++ MacroSubmitter.py
@@ -13,9 +13,9 @@ hr_start = 9    #Hit region. 1 = Ring 1, 2 = Ring 2, 3 = Ring 3, 4 = Ring 4, 5 =
 hr_stop = 9
 hr_step = 1     #Increments over each value of hr
 
-#cut_start = 0  #Breaks the quartz tiles up into multiple segments. How it is broken up is determined using hr. Not currently implemented for the 8 detector setup
-#cut_stop = 9
-#cut_step = 1
+cut_start = 0  #Keep start = stop unless hr = 10. Otherwise, multiple identical files will be created.
+cut_stop = 0
+cut_step = 1
 
 sa_start = 0    #Controls the angular spread of the beam from the Z-axis (in +- degrees). May be removed and set in src/...PrimaryGeneratorAction.cc for new UMass cosmic stand
 sa_stop = 0
@@ -28,30 +28,30 @@ ID_step = 1
 for hr in np.arange(hr_start,hr_stop+hr_step,hr_step):
     for sa in np.arange(sa_start,sa_stop+sa_step,sa_step):
         for id in np.arange(ID_start,ID_stop+ID_step,ID_step):
-            FileIDString = "_sa"+str(sa)+"_hR"+str(hr)
-            rootfile = "_sa"+str(sa)+"_hR"+str(hr)+".root"
-            jobs="jobs"
-            if not os.path.exists(jobs):
-                os.system("mkdir "+jobs)
-            
-            outDir = "rootfiles/"
-            home = sourceDir
-            FileName="./R6ParamScan/"+OutputFilePrefix + FileIDString+".mac"
-            if os.path.exists(FileName):
-                jsubf=open(jobs+"/"+OutputFilePrefix + FileIDString+".sh", "w")
-                jsubf.write("#!/bin/bash\n")
-                #jsubf.write("#SBATCH --account=halla\n")
-                jsubf.write("#SBATCH --partition=mocha\n")
-                jsubf.write("#SBATCH --job-name=PMT_EP\n")
-                jsubf.write("#SBATCH --output=out.out\n")
-                jsubf.write("#SBATCH --error=e.err\n")
-                jsubf.write("#SBATCH --time=24:00:00\n")
-                jsubf.write("#SBATCH --nodes=1\n")
-                jsubf.write("#SBATCH --ntasks=1\n")
-                jsubf.write("#SBATCH --cpus-per-task=1\n")
-                jsubf.write("#SBATCH --mem=40G\n")
-                jsubf.write("cd "+home+"\n")
-                jsubf.write("echo \"Current working directory is `pwd`\"\n")	
-                jsubf.write("./MOLLEROpt "+FileName+"\n")
-                jsubf.write("mv "+rootfile+" "+outDir+rootfile+"\n")
-                print("sbatch "+jobs+"/"+OutputFilePrefix + FileIDString+".sh")
+            for cut in np.arange(cut_start,cut_stop+cut_step,cut_step):
+                FileIDString = "_sa"+str(sa)+"_hR"+str(hr)+"_cut"+str(cut)
+                rootfile = "_sa"+str(sa)+"_hR"+str(hr)+"_cut"+str(cut)+".root"
+                jobs="jobs"
+                if not os.path.exists(jobs):
+                    os.system("mkdir "+jobs)
+                    outDir = "rootfiles/"
+                    home = sourceDir
+                    FileName="./R6ParamScan/"+OutputFilePrefix + FileIDString+".mac"
+                    if os.path.exists(FileName):
+                    jsubf=open(jobs+"/"+OutputFilePrefix + FileIDString+".sh", "w")
+                    jsubf.write("#!/bin/bash\n")
+                    #jsubf.write("#SBATCH --account=halla\n")
+                    jsubf.write("#SBATCH --partition=mocha\n")
+                    jsubf.write("#SBATCH --job-name=PMT_EP\n")
+                    jsubf.write("#SBATCH --output=out.out\n")
+                    jsubf.write("#SBATCH --error=e.err\n")
+                    jsubf.write("#SBATCH --time=24:00:00\n")
+                    jsubf.write("#SBATCH --nodes=1\n")
+                    jsubf.write("#SBATCH --ntasks=1\n")
+                    jsubf.write("#SBATCH --cpus-per-task=1\n")
+                    jsubf.write("#SBATCH --mem=40G\n")
+                    jsubf.write("cd "+home+"\n")
+                    jsubf.write("echo \"Current working directory is `pwd`\"\n")	
+                    jsubf.write("./MOLLEROpt "+FileName+"\n")
+                    jsubf.write("mv "+rootfile+" "+outDir+rootfile+"\n")
+                    print("sbatch "+jobs+"/"+OutputFilePrefix + FileIDString+".sh")

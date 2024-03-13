97d0d090d7dd78d8d7d1c9da45ab18afe7a6e830
Jonmott2u4u <jmott@umass.edu>
2024-03-13 18:56:10 -0400

    Reenabled position scans over the quartz tiles.

--- MacroSubmitter.py
+++ MacroSubmitter.py
@@ -8,12 +8,12 @@ sourceDir = "./"
 datadir =  "R6ParamScan/"
 OutputFilePrefix = "MOLLEROpt_Scan"
 
-hr_start = 5    #See macros for hr info
-hr_stop = 5
+hr_start = 8    #See macros for hr info
+hr_stop = 8
 hr_step = 1     #Increments over each value of hr
 
-cut_start = 10  #Breaks the quartz tiles up into multiple segments. How it is broken up is determined using hr. Not currently implemented for the 8 detector setup
-cut_stop = 10
+cut_start = 0  #Breaks the quartz tiles up into multiple segments. How it is broken up is determined using hr. Not currently implemented for the 8 detector setup
+cut_stop = 19
 cut_step = 1
 
 sa_start = 0    #Controls the angular spread of the beam from the Z-axis (in +- degrees). May be removed and set in src/...PrimaryGeneratorAction.cc for new UMass cosmic stand
@@ -32,41 +32,42 @@ for hr in np.arange(hr_start,hr_stop+hr_step,hr_step):
     for sa in np.arange(sa_start,sa_stop+sa_step,sa_step):
         for id in np.arange(ID_start,ID_stop+ID_step,ID_step):
             for det in np.arange(det_start,det_stop+det_step,det_step):
-                FileIDString = "_sa"+str(sa)+"_hR"+str(hr)+"_det"+str(det)+"_ID"+str(id)
-                rootfile = FileIDString + ".root"
-                jobs="jobs"
-                if not os.path.exists(jobs):
-                    os.system("mkdir "+jobs)
-                outDir = "rootfiles/"
-                home = sourceDir
-                FileName="./R6ParamScan/"+OutputFilePrefix + FileIDString+".mac"
-                if os.path.exists(FileName):
-                    jsubf=open(jobs+"/"+OutputFilePrefix + FileIDString+".sh", "w")
-                    jsubf.write("#!/bin/bash\n")
-                    #---Submission info for Mocha---
-                    jsubf.write("#SBATCH --partition=mocha\n")
-                    jsubf.write("#SBATCH --job-name=PMT_EP\n")
-                    jsubf.write("#SBATCH --output=out.out\n")
-                    jsubf.write("#SBATCH --error=e.err\n")
-                    jsubf.write("#SBATCH --time=24:00:00\n")
-                    jsubf.write("#SBATCH --nodes=1\n")
-                    jsubf.write("#SBATCH --ntasks=1\n")
-                    jsubf.write("#SBATCH --cpus-per-task=1\n")
-                    jsubf.write("#SBATCH --mem=40G\n")
-                    #---Submission info for IFarm---
-                    #jsubf.write("#SBATCH --account=halla\n")
-                    #jsubf.write("#SBATCH --partition=production\n")
-                    #jsubf.write("#SBATCH --job-name=PMT_EP\n")
-                    #jsubf.write("#SBATCH --output=/farm_out/%u/%x-%j-%N.out\n")
-                    #jsubf.write("#SBATCH --error=/farm_out/%u/%x-%j-%N.err\n")
-                    #jsubf.write("#SBATCH --time=24:00:00\n")
-                    #jsubf.write("#SBATCH --nodes=1\n")
-                    #jsubf.write("#SBATCH --ntasks=1\n")
-                    #jsubf.write("#SBATCH --cpus-per-task=1\n")
-                    #jsubf.write("#SBATCH --mem=80G\n")
-                    #---General submission info---
-                    jsubf.write("cd "+home+"\n")
-                    jsubf.write("echo \"Current working directory is `pwd`\"\n")	
-                    jsubf.write("./MOLLEROpt "+FileName+"\n")
-                    jsubf.write("mv "+rootfile+" "+outDir+rootfile+"\n")
-                    print("sbatch "+jobs+"/"+OutputFilePrefix + FileIDString+".sh")
+                for cut in np.arange(cut_start,cut_stop+cut_step,cut_step):
+                    FileIDString = "_sa"+str(sa)+"_hR"+str(hr)+"_det"+str(det)+"_cut"+str(cut)+"_ID"+str(id)
+                    rootfile = FileIDString + ".root"
+                    jobs="jobs"
+                    if not os.path.exists(jobs):
+                        os.system("mkdir "+jobs)
+                    outDir = "rootfiles/"
+                    home = sourceDir
+                    FileName="./R6ParamScan/"+OutputFilePrefix + FileIDString+".mac"
+                    if os.path.exists(FileName):
+                        jsubf=open(jobs+"/"+OutputFilePrefix + FileIDString+".sh", "w")
+                        jsubf.write("#!/bin/bash\n")
+                        #---Submission info for Mocha---
+                        #jsubf.write("#SBATCH --partition=mocha\n")
+                        #jsubf.write("#SBATCH --job-name=PMT_EP\n")
+                        #jsubf.write("#SBATCH --output=out.out\n")
+                        #jsubf.write("#SBATCH --error=e.err\n")
+                        #jsubf.write("#SBATCH --time=24:00:00\n")
+                        #jsubf.write("#SBATCH --nodes=1\n")
+                        #jsubf.write("#SBATCH --ntasks=1\n")
+                        #jsubf.write("#SBATCH --cpus-per-task=1\n")
+                        #jsubf.write("#SBATCH --mem=40G\n")
+                        #---Submission info for IFarm---
+                        jsubf.write("#SBATCH --account=halla\n")
+                        jsubf.write("#SBATCH --partition=production\n")
+                        jsubf.write("#SBATCH --job-name=PMT_EP\n")
+                        jsubf.write("#SBATCH --output=/farm_out/%u/%x-%j-%N.out\n")
+                        jsubf.write("#SBATCH --error=/farm_out/%u/%x-%j-%N.err\n")
+                        jsubf.write("#SBATCH --time=24:00:00\n")
+                        jsubf.write("#SBATCH --nodes=1\n")
+                        jsubf.write("#SBATCH --ntasks=1\n")
+                        jsubf.write("#SBATCH --cpus-per-task=1\n")
+                        jsubf.write("#SBATCH --mem=80G\n")
+                        #---General submission info---
+                        jsubf.write("cd "+home+"\n")
+                        jsubf.write("echo \"Current working directory is `pwd`\"\n")	
+                        jsubf.write("./MOLLEROpt "+FileName+"\n")
+                        jsubf.write("mv "+rootfile+" "+outDir+rootfile+"\n")
+                        print("sbatch "+jobs+"/"+OutputFilePrefix + FileIDString+".sh")

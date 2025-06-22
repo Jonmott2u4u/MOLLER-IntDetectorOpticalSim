108df78c04be64050cf12eb878542f900bf0bc3a
jon <jonmott46@gmail.com>
2025-06-22 19:43:03 -0400

    Files are no longer saved en-masse, but removed multi detector scanning from script

--- PlotExtractor.C
+++ PlotExtractor.C
@@ -252,27 +252,27 @@ void quartz_hit_pos(){
     string location = "MOLLEROptData.MOLLERGeneralEvent.";
     TFile *file;
     TH1D *hst, *tmp;
+    int det = 5;
+
+    TCanvas *canvas1 = new TCanvas("canvas1","canvas1");
 
     while(std::getline(rfiles, line)){
         tmpStr = line.data();
         tmpStr = tmpStr.ReplaceAll(".root",""); 
         file = TFile::Open(line.data());
-        for(int det=5; det<6; det++){
-            
-            TTree *tree = (TTree*)file->Get("MOLLEROptTree");
-            //cout << tree << endl;
-            TCanvas *canvas1 = new TCanvas("canvas1","canvas1");
-            tree->Draw(Form("%sR%iTileHitY:%sR%iTileHitX",location.data(),det,location.data(),det),Form("%sScint2TrackHit==1 && %sScint4TrackHit==1",location.data(),location.data()),"colz");
-            canvas1->SaveAs(Form("plots/file_%s_R%i_hit_pos.root",tmpStr.Data(),det));
+        TTree *tree = (TTree*)file->Get("MOLLEROptTree");
 
-            /*TCanvas *canvas2 = new TCanvas("canvas2","canvas2");
-            tree->Draw(Form("%sR%iTileHitY:%sR%iTileHitX",location.data(),det,location.data(),det),Form("%sScint2TrackHit==1 && %sScint4TrackHit==1 && %sR4QuartzTrackHit==0 && %sR8QuartzTrackHit==0",location.data(),location.data(),location.data(),location.data()),"colz");
-            canvas2->SaveAs(Form("plots/R%i_adjacency_quartz_hit_pos.root",det));
+        tree->Draw(Form("%sR%iTileHitY:%sR%iTileHitX",location.data(),det,location.data(),det),Form("%sScint2TrackHit==1 && %sScint4TrackHit==1",location.data(),location.data()),"colz");
+        //canvas1->SaveAs(Form("plots/file_%s_R%i_hit_pos.root",tmpStr.Data(),det));
 
-            TCanvas *canvas3 = new TCanvas("canvas3","canvas3");
-            tree->Draw(Form("%sR%iTileHitY:%sR%iTileHitX",location.data(),det,location.data(),det),Form("%sScint2TrackHit==1 && %sScint4TrackHit==1 && %sR1QuartzTrackHit==0 && %sR2QuartzTrackHit==0 && %sR3QuartzTrackHit==0 && %sR4QuartzTrackHit==0 && %sR6QuartzTrackHit==0 && %sR7QuartzTrackHit==0 && %sR8QuartzTrackHit==0",location.data(),location.data(),location.data(),location.data(),location.data(),location.data(),location.data(),location.data(),location.data()),"colz");
-            canvas3->SaveAs(Form("plots/R%i_solo_quartz_hit_pos.root",det));*/
+        /*TCanvas *canvas2 = new TCanvas("canvas2","canvas2");
+        tree->Draw(Form("%sR%iTileHitY:%sR%iTileHitX",location.data(),det,location.data(),det),Form("%sScint2TrackHit==1 && %sScint4TrackHit==1 && %sR4QuartzTrackHit==0 && %sR8QuartzTrackHit==0",location.data(),location.data(),location.data(),location.data()),"colz");
+        canvas2->SaveAs(Form("plots/R%i_adjacency_quartz_hit_pos.root",det));
 
-        }
+        TCanvas *canvas3 = new TCanvas("canvas3","canvas3");
+        tree->Draw(Form("%sR%iTileHitY:%sR%iTileHitX",location.data(),det,location.data(),det),Form("%sScint2TrackHit==1 && %sScint4TrackHit==1 && %sR1QuartzTrackHit==0 && %sR2QuartzTrackHit==0 && %sR3QuartzTrackHit==0 && %sR4QuartzTrackHit==0 && %sR6QuartzTrackHit==0 && %sR7QuartzTrackHit==0 && %sR8QuartzTrackHit==0",location.data(),location.data(),location.data(),location.data(),location.data(),location.data(),location.data(),location.data(),location.data()),"colz");
+        canvas3->SaveAs(Form("plots/R%i_solo_quartz_hit_pos.root",det));*/
+
     }
+    canvas1->SaveAs(Form("plots/file_%s_R%i_hit_pos.root",tmpStr.Data(),det));
 }
